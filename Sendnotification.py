5665cbe4ea6375f628d6f992788ad585bc9796a3
himavanthv <113790263+himavanthv@users.noreply.github.com>
2025-12-14 16:24:48 +0530

    Update Sendnotification.py

--- Sendnotification.py
+++ Sendnotification.py
@@ -6,12 +6,13 @@ def send_telegram_notification(message_text):
 
     with open('config.json', 'r') as f:
         config = json.load(f)
-
-    url = f"https://api.telegram.org/bot{config['BOT_TOKEN']}/sendMessage"
+    bot_token = os.getenv("TELEGRAM_API_TOKEN")
+    chat_id = os.getenv("CHAT_ID")
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     
 
     params = {
-        "chat_id": config['CHAT_ID'],
+        "chat_id": chat_id,
         "text": message_text,
         "parse_mode": "Markdown" 
     }
@@ -28,4 +29,5 @@ def send_telegram_notification(message_text):
 
 if __name__ == "__main__":
     notification_message = "Hello from your Python script! This is a **notification** with *Markdown* formatting and a [link to the docs](core.telegram.org)."
+
     send_telegram_notification(notification_message)
