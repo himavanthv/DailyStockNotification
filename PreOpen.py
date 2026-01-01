28abfc6e0aa310620aa5837e553bcd9f2ba5ad0c
himavanthv <113790263+himavanthv@users.noreply.github.com>
2026-01-01 22:48:25 +0530

    Add files via upload

--- PreOpen.py
+++ PreOpen.py
@@ -2,7 +2,9 @@ from nsepython import nse_preopen
 from nsepython import nse_preopen_movers
 from datetime import datetime
 import pandas as pd
+from nsepython import nse_get_top_gainers, nse_get_top_losers
 from Sendnotification import send_telegram_notification
+
 def PreOpen():
     nifty_preopen_data = nse_preopen(key="ALL", type="pandas")
     current_datetime = datetime.now()
@@ -14,4 +16,13 @@ def PreOpen():
     morethan1pcchange = abs(stockstobeconsidered['pChange']) > 1
     finalobservationstocks = stockstobeconsidered[morethan1pcchange]
     return finalobservationstocks
+
+def get_top_gainers():
+    gainers = nse_get_top_gainers()
+    return gainers
+
+def get_top_losers():
+    losers = nse_get_top_losers()
+    return losers
+
 
