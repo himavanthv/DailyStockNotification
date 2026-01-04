ec426683fe9752ded1a02b62436164750a477809
himavanthv <113790263+himavanthv@users.noreply.github.com>
2026-01-04 21:55:53 +0530

    Add files via upload

--- ModelBuilder.py
+++ ModelBuilder.py
@@ -7,24 +7,31 @@ from Sendnotification import send_telegram_notification
 from datetime import datetime
 
 def getoneminutedata(ticker_symbol):
-    data=nse.fetch_data(ticker_symbol+".NS","30m","30d")
+    data=nse.fetch_data(ticker_symbol+".NS","15m","15d")
     return  data
 
 
 def performanalysis(data):
     data.dropna(inplace=True)
     dataLen=len(data)
-    prev_short = data['SMA_9'].iloc[2]
-    prev_long = data['SMA_26'].iloc[2]
-    curr_short = data['SMA_9'].iloc[1]
-    curr_long = data['SMA_26'].iloc[1]
+    prev_short = data['SMA_9'].iloc[1]
+    prev_long = data['SMA_26'].iloc[1]
+    curr_short = data['SMA_9'].iloc[0]
+    curr_long = data['SMA_26'].iloc[0]
     latestshort = data['SMA_9'].iloc[0]
     latestlong = data['SMA_26'].iloc[0]
-    previousclose=data['Close'].iloc[1]
-    percentema=((latestshort-latestlong)/latestshort)*100
-    if prev_short <= prev_long and curr_short > curr_long and previousclose>curr_short and percentema>0.15:
+    currropen=data['Open'].iloc[0]
+    prevclose=data['Close'].iloc[1]
+    prevopen=data['Open'].iloc[1]
+    currrsi=data['RSI_14'].iloc[0]
+    prevrsi1=data['RSI_14'].iloc[1]
+    prevrsi2=data['RSI_14'].iloc[2]
+    prevrsi3=data['RSI_14'].iloc[3]
+    prevchange = ((prevclose-prevopen)/prevopen)*100
+    percentma=((latestshort-latestlong)/latestshort)*100
+    if prev_short <= prev_long and curr_short > curr_long and currropen>curr_short and currrsi>prevrsi1 and prevrsi1>prevrsi2 and prevrsi2>prevrsi3 and currrsi>60:
         return "BULLISH Crossover detected on the current (most recent closed) candle."
-    elif prev_short >= prev_long and curr_short < curr_long and previousclose<curr_short and percentema<-0.15:
+    elif prev_short >= prev_long and curr_short < curr_long and currropen<curr_short and currrsi<prevrsi1 and prevrsi1<prevrsi2 and prevrsi2<prevrsi3 and currrsi<40:
         return "BEARISH Crossover detected on the current (most recent closed) candle."
     else:
         return "No new crossover detected on the latest candle."
@@ -33,6 +40,7 @@ ist_timezone = pytz.timezone('Asia/Kolkata')
 allstocks = pd.read_csv('AllOptionsStocks.csv')
 
 for i in range(1):
+    #print('Start Time '+datetime.now(ist_timezone).strftime("%H:%M:%S"))
     datafornotification = ""
     datafornotification = pd.DataFrame(columns=['Stock Symbol','Analysis'])
     for index, row in allstocks.iterrows():
@@ -49,4 +57,6 @@ for i in range(1):
     markdown_msg = datafornotification.to_markdown(index=False)
     formatted_payload = f"```\n{markdown_msg}\n```"
     current_time_ist = datetime.now(ist_timezone)
+    print('End Time '+datetime.now(ist_timezone).strftime("%H:%M:%S"))
+    #print(datafornotification)
     send_telegram_notification("Analysis report at Time:"+ current_time_ist.strftime("%H:%M:%S") +"\n"+formatted_payload)
